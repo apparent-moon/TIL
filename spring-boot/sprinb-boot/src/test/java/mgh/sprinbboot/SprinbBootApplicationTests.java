2130396090d98926cc949b20f118834a2f2b3cfc
apmoon <ohappy0206@naver.com>
2024-05-27 16:11:24 +0900

    apmoon) springboot practice project create 20240527 16:11

---
+++ spring-boot/sprinb-boot/src/test/java/mgh/sprinbboot/SprinbBootApplicationTests.java
@@ -0,0 +1,13 @@
+package mgh.sprinbboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
