ed510964534046dd3126b7c2d91d382e54caee06
apmoon <apmoon203@gmail.com>
2024-08-21 20:37:39 +0900

    apmoon) JS Study 240821 20:37

--- JavaScript/tab.js
+++ JavaScript/tab.js
@@ -20,7 +20,7 @@ $('.tab-button').eq(2).on('click', function(){
     $('.tab-button').eq(2).addClass('orange');
     $('.tab-content').removeClass('show');
     $('.tab-content').eq(2).addClass('show');
-}) */
+})
 
 for(let i = 0; i < 3; i++){
     $('.tab-button').eq(i).on('click', function() {
@@ -31,7 +31,6 @@ for(let i = 0; i < 3; i++){
     });
 };
 
-/*
 for (let i = 0; i < $('.tab-button').length; i++){
 
     $('.tab-button').eq(i).on('click', function(){
@@ -43,4 +42,30 @@ for (let i = 0; i < $('.tab-button').length; i++){
 
 };
 
+
+for(let i = 0; i < 3; i++){
+    $('.tab-button').eq(i).on('click', function() {
+        tabOpen(i);
+    });
+};
+
+$('.list').click(function(e){
+    if(e.target == document.querySelectorAll('.tab-button')[0]){
+        tabOpen(0);
+    }else if(e.target == document.querySelectorAll('.tab-button')[1]){
+        tabOpen(1);
+    }else if(e.target == document.querySelectorAll('.tab-button')[2]){
+        tabOpen(2);
+    }
+});
 */
+$('.list').click(function(e){
+    tabOpen(e.target.dataset.id)
+});
+
+function tabOpen(i){
+    tabButton.removeClass('orange');
+    tabButton.eq(i).addClass('orange');
+    tabContent.removeClass('show');
+    tabContent.eq(i).addClass('show');
+};
