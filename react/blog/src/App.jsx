98140744d2d8216ec278ad12dfacd6e8188bc2ae
apmoon <apmoon203@gmail.com>
2025-01-18 18:19:18 +0900

    apmoon) react study 250118 18:19

--- react/blog/src/App.jsx
+++ react/blog/src/App.jsx
@@ -1,15 +1,28 @@
 import './App.css'
+import {useState} from "react";
 
 function App() {
 
-    let post = '변수';
+    let post = '강남 우동 맛집';
+    let [title, d] = useState(['여자코트추천', '맛집추천', '우동맛집추천']);
 
   return (
       <div className="App">
           <div className="black-nav">
-              <h4 style={ {color:'red', fontSize : '20px'} }>Blog</h4>
+              <h4 style={{color: 'blueviolet', fontSize: '20px'}}>Blog</h4>
           </div>
-          <h4>{post}</h4>
+          <div className="list">
+              <h4>{title[0]}</h4>
+              <p>1월 17일 발행</p>
+          </div>
+          <div className="list">
+              <h4>{title[1]}</h4>
+              <p>1월 17일 발행</p>
+          </div>
+          <div className="list">
+              <h4>{title[2]}</h4>
+              <p>1월 17일 발행</p>
+          </div>
       </div>
   )
 }
